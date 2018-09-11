3b82e97600f99b3745583a43070689542f243111
Stephen Franceschelli <stephenmichaelf@gmail.com>
2018-09-11 19:53:53 -0400

    Add logging.

--- src/languageservice/parser/jsonParser.ts
+++ src/languageservice/parser/jsonParser.ts
@@ -984,30 +984,41 @@ class SchemaCollector implements ISchemaCollector {
 		if ((schema.node.type === 'string' || schema.node.type === 'null')
 			&& schema.schema.enum && schema.schema.enum.length > 20){
 				// The null one does get added! It must be getting filtered somewhere...
-				logger.log(`SCHEMACOLLECTOR.ADD, type: ${schema.node.type} schema.enum.length: ${schema.schema.enum.length}`)
+				logger.log(`SCHEMACOLLECTOR.ADD, type: ${schema.node.type} schema.enum.length: ${schema.schema.enum.length}, containsEnumList: ${this.containsEnumList()}`)
 			}
 	}
 	merge(other: ISchemaCollector) {
-		logger.log('SCHEMACOLLECTOR.MERGE');
-		logger.log(`SCHEMACOLLECTOR.MERGE- length before: ${this.schemas.length}`);
+		logger.log('SCHEMACOLLECTOR.MERGE, containsEnumList: ${this.containsEnumList()}');
+		logger.log(`SCHEMACOLLECTOR.MERGE- length before: ${this.schemas.length}, containsEnumList: ${this.containsEnumList()}`);
 		this.schemas.push(...other.schemas);
-		logger.log(`SCHEMACOLLECTOR.MERGE- length after: ${this.schemas.length}`);
+		logger.log(`SCHEMACOLLECTOR.MERGE- length after: ${this.schemas.length}, containsEnumList: ${this.containsEnumList()}`);
 	}
 	include(node: ASTNode) {
-		logger.log('SCHEMACOLLECTOR.include');
+		logger.log('SCHEMACOLLECTOR.include, containsEnumList: ${this.containsEnumList()}');
 
 		const result: boolean = (this.focusOffset === -1 || node.contains(this.focusOffset)) && (node !== this.exclude);
 
-		logger.log(`SCHEMACOLLECTOR.newSub- boolean result: ${result}`);
+		logger.log(`SCHEMACOLLECTOR.newSub- boolean result: ${result}, containsEnumList: ${this.containsEnumList()}`);
 
 		return result;
 	}
 	newSub(): ISchemaCollector {
-		logger.log('SCHEMACOLLECTOR.newSub');
-		logger.log(`SCHEMACOLLECTOR.newSub- length before: ${this.schemas.length}`);
+		logger.log('SCHEMACOLLECTOR.newSub, containsEnumList: ${this.containsEnumList()}');
+		logger.log(`SCHEMACOLLECTOR.newSub- length before: ${this.schemas.length}, containsEnumList: ${this.containsEnumList()}`);
 		const newSchemaCollector = new SchemaCollector(-1, this.exclude);
-		logger.log(`SCHEMACOLLECTOR.newSub- length before: ${newSchemaCollector.schemas.length}`);
+		logger.log(`SCHEMACOLLECTOR.newSub- length before: ${newSchemaCollector.schemas.length}, containsEnumList: ${this.containsEnumList()}`);
 		return newSchemaCollector;
+	}
+
+	private containsEnumList(): boolean {
+		for (var i = 0; i < this.schemas.length; i++){
+			if ((this.schemas[i].node.type === 'string' || this.schemas[i].node.type === 'null')
+				&& this.schemas[i].schema.enum && this.schemas[i].schema.enum.length > 20) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
 
