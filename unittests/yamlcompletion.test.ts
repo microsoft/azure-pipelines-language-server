1f19e38873552fa8f5db50769a8a47ca818350bc
Stephen Franceschelli <Stephen.Franceschelli@microsoft.com>
2018-09-11 10:02:32 -0400

    Cleanup.

--- unittests/yamlcompletion.test.ts
+++ unittests/yamlcompletion.test.ts
@@ -15,6 +15,55 @@ import * as util from 'util';
 
 const schemaUri: string = "file:///E%3A/ExtensionLearning/azure-pipelines-language-server/unittests/schema.json";
 
+suite("Yaml Completion Service Tests", function() {
+    this.timeout(20000);
+
+    // test ('Given an already valid file with task name, autocomplete should give suggestions', async function() {
+    //     await runTaskCompletionItemsTest('steps:\n- task: npmAuthenticate@0', 1, 7, 160);
+    // });
+
+    // test ('Given a new file with steps and task, autocomplete should give suggestions', async function() {
+    //     // TODO: We actually want expectedTaskCount to be 160, not 0. This is the bug.
+    //     await runTaskCompletionItemsTest('steps:\n- task: ', 1, 7, 0);
+    // });
+});
+
+suite("Validate matching schemas for document", function() {
+    this.timeout(20000);
+
+    test ('When the document has a string final node, we should see matching schemas for tasks', async function() {
+        // Arrange
+        const jsonDocument: JSONDocument = getJsonDocument('steps:\n- task: npmAuthenticate@0');
+        const jsonSchemaService = new JSONSchemaService.JSONSchemaService(null);
+        const schema: JSONSchemaService.ResolvedSchema = await jsonSchemaService.resolveSchemaContent(getSchema(), schemaUri);
+
+        // Act
+        const matchingSchemas: IApplicableSchema[] = jsonDocument.getMatchingSchemas(schema.schema);
+
+        // Assert
+        assert.equal(matchingSchemas.length, 8);
+    });
+
+    // This is the one that currently fails downstream. We don't see matching schemas for tasks when we should.
+    // We get 4 matches instead of 8, and the match to show the task enum list isn't there.
+    test ('When the document has a null final node, we should see matching schemas for tasks', async function() {
+        // Arrange
+        const jsonDocument: JSONDocument = getJsonDocument('steps:\n- task: ');
+        const jsonSchemaService = new JSONSchemaService.JSONSchemaService(null);
+        const schema: JSONSchemaService.ResolvedSchema = await jsonSchemaService.resolveSchemaContent(getSchema(), schemaUri);
+
+        // Act
+        const matchingSchemas: IApplicableSchema[] = jsonDocument.getMatchingSchemas(schema.schema);
+
+        // Assert
+        assert.equal(matchingSchemas.length, 8);
+    });
+});
+
+/**
+ * Helpers.
+ */
+
 let workspaceContext = {
 	resolveRelativePath: (relativePath: string, resource: string) => {
 		return URL.resolve(resource, relativePath);
@@ -36,19 +85,6 @@ const requestService = (uri: string): Thenable<string> => {
     }
 };
 
-suite("Yaml Completion Service Tests", function() {
-    this.timeout(20000);
-
-    test ('Given an already valid file with task name, autocomplete should give suggestions', async function() {
-        await runTaskCompletionItemsTest('steps:\n- task: npmAuthenticate@0', 1, 7, 160);
-    });
-
-    // test ('Given a new file with steps and task, autocomplete should give suggestions', async function() {
-    //     // TODO: We actually want expectedTaskCount to be 160, not 0. This is the bug.
-    //     await runTaskCompletionItemsTest('steps:\n- task: ', 1, 7, 0);
-    // });
-});
-
 function getJsonDocument(content: string): JSONDocument {
     // we are just copying the structure of his code for now, for simplicity
     
@@ -62,30 +98,6 @@ function getJsonDocument(content: string): JSONDocument {
 
     return currentDoc;
 }
-
-suite("Validate matching schemas for document", function() {
-    this.timeout(20000);
-
-    test ('When the document has a string final node, we should see matching schemas for tasks', async function() {
-        // Arrange
-        const jsonDocument: JSONDocument = getJsonDocument('steps:\n- task: npmAuthenticate@0');
-        const jsonSchemaService = new JSONSchemaService.JSONSchemaService(null);
-        const schema: JSONSchemaService.ResolvedSchema = await jsonSchemaService.resolveSchemaContent(getSchema(), schemaUri);
-
-        // Act
-        const matchingSchemas: IApplicableSchema[] = jsonDocument.getMatchingSchemas(schema.schema);
-
-        // Assert
-        assert.equal(matchingSchemas.length, 8);
-    });
-
-    // test ('When the document has a null final node, we should see matching schemas for tasks', async function() {
-    //     // TODO: This is the one that currently fails downstream. We don't see matching schemas for tasks when we should.
-    // });
-
-    // test ('', async function() {
-    // });
-});
 
 function getSchema(): JSONSchemaService.ResolvedSchema {
     const schemaText: string = fs.readFileSync('unittests/schema.json', 'utf8');
