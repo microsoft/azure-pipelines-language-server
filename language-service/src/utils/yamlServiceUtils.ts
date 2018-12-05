609c17ba9c6d9e28471aab7f67b79af466fbfdea
Joshua Cole <joscol@microsoft.com>
2018-12-05 13:12:22 -0500

    reduce scope of changes

--- language-service/src/utils/yamlServiceUtils.ts
+++ language-service/src/utils/yamlServiceUtils.ts
@@ -37,19 +37,18 @@ export function completionHelper(document: TextDocument, textDocumentPosition: P
     if (textLine.indexOf(":") === -1) {
         // We need to add the ":" to load the nodes
         const documentText = document.getText();
-        let newText = documentText.substring(0, start + textDocumentPosition.character);
+
+        let newText = "";
 
         // This is for the empty line case
         const trimmedText = textLine.trim();
         if (trimmedText.length === 0 || (trimmedText.length === 1 && trimmedText[0] === '-')) {
             // Add a temp node that is in the document but we don't use at all.
-            newText += nodeHolderWithEnding;
+            newText = documentText.substring(0, start + textDocumentPosition.character) + nodeHolderWithEnding + documentText.substr(start + textDocumentPosition.character);
         } else {
-            // Add a semicolon to the end of the current line so we can validate the node
-            newText += nodeLineEnding;
+            // Add a colon to the end of the current line so we can validate the node
+            newText = documentText.substring(0, start + textLine.length) + nodeLineEnding + documentText.substr(lineOffsets[lineNumber + 1] || documentText.length);
         }
-
-        newText += documentText.substr(start + textDocumentPosition.character);
 
         return {
             newText: newText,
