7f4d751e410aeb97e4fde3ff6eb601b438305def
Joshua Cole <joscol@microsoft.com>
2018-12-06 13:39:53 -0500

    remove more literal offsets

--- language-server/test/autoCompletion.test.ts
+++ language-server/test/autoCompletion.test.ts
@@ -28,103 +28,107 @@ suite("Auto Completion Tests", () => {
 		describe('doComplete', function(){
 
 			function parseSetup(content: string, position){
-				let document = TextDocument.create("file://~/Desktop/vscode-k8s/test.yaml", "yaml", 0, content);
-				let completion = completionHelper(document, document.positionAt(position));
-				let jsonDocument = parseYAML(completion.newText);
+				const document = TextDocument.create("file://~/Desktop/vscode-k8s/test.yaml", "yaml", 0, content);
+				const completion = completionHelper(document, document.positionAt(position));
+				const jsonDocument = parseYAML(completion.newText);
 				return languageService.doComplete(document, completion.newPosition, jsonDocument);
 			}
 
 			it('Autocomplete on root node without word', (done) => {
-				let content = "";
-				let completion = parseSetup(content, 0);
+				const content: string = "";
+				const completion = parseSetup(content, content.length);
 				completion.then(function(result){
                     assert.notEqual(result.items.length, 0);
 				}).then(done, done);
 			});
 
 			it('Autocomplete on root node with word', (done) => {
-				let content = "analyt";
-				let completion = parseSetup(content, 6);
+				const content: string = "analyt";
+				const completion = parseSetup(content, content.length);
 				completion.then(function(result){
 					assert.notEqual(result.items.length, 0);
 				}).then(done, done);
 			});
 
 			it('Autocomplete on default value (without value content)', (done) => {
-				let content = "directory: ";
-				let completion = parseSetup(content, 12);
+				const content: string = "directory: ";
+				const completion = parseSetup(content, content.length);
 				completion.then(function(result){
 					assert.notEqual(result.items.length, 0);
 				}).then(done, done);
 			});
 
 			it('Autocomplete on default value (with value content)', (done) => {
-				let content = "directory: bow";
-				let completion = parseSetup(content, 15);
+				const content: string = "directory: bow";
+				const completion = parseSetup(content, content.length);
 				completion.then(function(result){
 					assert.notEqual(result.items.length, 0);
 				}).then(done, done);
 			});
 
 			it('Autocomplete on boolean value (without value content)', (done) => {
-				let content = "analytics: ";
-				let completion = parseSetup(content, 11);
+				const content: string = "analytics: ";
+				const completion = parseSetup(content, content.length);
 				completion.then(function(result){
 					assert.equal(result.items.length, 2);
 				}).then(done, done);
 			});
 
 			it('Autocomplete on boolean value (with value content)', (done) => {
-				let content = "analytics: fal";
-				let completion = parseSetup(content, 11);
+				const content: string = "analytics: fal";
+				const completion = parseSetup(content, content.length);
 				completion.then(function(result){
 					assert.equal(result.items.length, 2);
 				}).then(done, done);
 			});
 
 			it('Autocomplete on number value (without value content)', (done) => {
-				let content = "timeout: ";
-				let completion = parseSetup(content, 9);
+				const content: string = "timeout: ";
+				const completion = parseSetup(content, content.length);
 				completion.then(function(result){
 					assert.equal(result.items.length, 1);
 				}).then(done, done);
 			});
 
 			it('Autocomplete on number value (with value content)', (done) => {
-				let content = "timeout: 6";
-				let completion = parseSetup(content, 10);
+				const content: string = "timeout: 6";
+				const completion = parseSetup(content, content.length);
 				completion.then(function(result){
 					assert.equal(result.items.length, 1);
 				}).then(done, done);
 			});
 
 			it('Autocomplete key in middle of file', (done) => {
-				let content = "scripts:\n  post";
-				let completion = parseSetup(content, 11);
+				const preCursorContent: string = "scripts:\n  po";
+				const postCursorConent: string = "st";
+				const completion = parseSetup(preCursorContent + postCursorConent, preCursorContent.length);
 				completion.then(function(result){
 					assert.notEqual(result.items.length, 0);
 				}).then(done, done);
 			});
 
 			it('Autocomplete key in middle of file 2', (done) => {
-				let content = "scripts:\n  postinstall: /test\n  preinsta";
-				let completion = parseSetup(content, 31);
+				const preCursorContent: string = "scripts:\n  postinstall: /test\n  pr"
+				const postCursorContent: string = "einsta";
+				const completion = parseSetup(preCursorContent + postCursorContent, preCursorContent.length);
 				completion.then(function(result){
 					assert.notEqual(result.items.length, 0);
 				}).then(done, done);
 			});
 
-			it('Autocomplete does not happen right after :', (done) => {
-				let content = "analytics:";
-				let completion = parseSetup(content, 9);
+			it('Autocomplete does not happen right before :', (done) => {
+				const preCursorContent: string = "analytics";
+				const postCursorContent: string = ":";
+				const completion = parseSetup(preCursorContent + postCursorContent, preCursorContent.length);
 				completion.then(function(result){
 					assert.notEqual(result.items.length, 0);
 				}).then(done, done);
 			});
 
-			it('Autocomplete does not happen right after : under an object', (done) => {
-				let content = "scripts:\n  postinstall:";
-				let completion = parseSetup(content, 21);
+			it('Autocomplete does not happen right before : under an object', (done) => {
+				const preCursorContent: string = "scripts:\n  postinstall";
+				const postCursorContent: string = ":";
+				const completion = parseSetup(preCursorContent + postCursorContent, preCursorContent.length);
 				completion.then(function(result){
 					assert.notEqual(result.items.length, 0);
 				}).then(done, done);
